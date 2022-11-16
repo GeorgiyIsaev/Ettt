47dea7efb84bfa5ce659daf995b888570cf1cc23
Георгий ГераМера <germesutan@gmail.com>
2022-11-16 22:08:13 +0300

    поток

--- src/ru/gb/Main.java
+++ src/ru/gb/Main.java
@@ -24,67 +24,69 @@ public class Main {
                 Socket socket = serverSocket.accept(); //Будит ожидать подключение клиента
                 System.out.println("New client connect");
 
-
-                //Получаемая от клиента информация socket.getInputStream() приходит в виде байтов
-                //Её можно преобразовать в читаемы вид через InputStreamReader
-                //класс BufferedReader позволяет считывать всю информацию в виде строки, а не символов
-                try (BufferedReader input = new BufferedReader(
-                        new InputStreamReader(
-                                socket.getInputStream(), StandardCharsets.UTF_8));
-                     PrintWriter output = new PrintWriter(socket.getOutputStream());) {
-
-                    //input.ready(); // ожидаем прихода информации
-
-                    while (!input.ready()) ; //ждем пока не появится что-то что можно считать
-
-                    //Создадим переменую для хранения запрашиваемого пользователем адреса
-                    String firstLine = input.readLine();
-                    String[] parts = firstLine.split(" ");
-                    System.out.println(firstLine); // выводим пока есть что выводить
-                    System.out.println(" parts[1]: " +  parts[1]); // выводим пока есть что выводить
-
-
-                    while (input.ready()) {
-                        System.out.println(input.readLine()); // выводим пока есть что выводить
-                    }
-
-
-                    parts[1] = parts[1].substring(0);
-                    Path path = Paths.get("C:\\REPOSITORY\\GB.JavaWeb.WebSerwer\\www\\", parts[1]);
-                    //Path path = Paths.get(".", parts[1]);
-                    if(!Files.exists(path)){
-                        output.println("HTTP/1.1 404 NOT_FOUND");
-                        output.println("Content-Type: text/html; charset=utf-8");
-                        output.println();
-                        output.println("<h1>Файл "+ parts[1] +" не найден</h1>"); //страница которую мы передаем
-                        output.flush();
-                        continue;
-                    }
-
-
-
-
-
-                    //Ответ (версия протокола) (код 200) (текстовое значение кода)
-                    //тип страницы и кодировка
-                    //Обязательно пустую строку в конце (ответ закончен)
-                    output.println("HTTP/1.1 200 OK");
-                    output.println("Content-Type: text/html; charset=utf-8");
-                    output.println();
-
-                    Files.newBufferedReader(path).transferTo(output);
-                    //output.println("<h1>Мой документ</h1>"); //страница которую мы передаем
-                    output.flush();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+                //Что бы каждый клиент работал со своим потоком
+                new Thread(() -> handleRequest(socket)).start();            }
 
 
         } catch (IOException e){
             e.printStackTrace();
         }
 
+
+
     }
+    private static void handleRequest(Socket socket) {
+        //Получаемая от клиента информация socket.getInputStream() приходит в виде байтов
+        //Её можно преобразовать в читаемы вид через InputStreamReader
+        //класс BufferedReader позволяет считывать всю информацию в виде строки, а не символов
+        try (BufferedReader input = new BufferedReader(
+                new InputStreamReader(
+                        socket.getInputStream(), StandardCharsets.UTF_8));
+             PrintWriter output = new PrintWriter(socket.getOutputStream());) {
+
+            //input.ready(); // ожидаем прихода информации
+
+            while (!input.ready()) ; //ждем пока не появится что-то что можно считать
+
+            //Создадим переменую для хранения запрашиваемого пользователем адреса
+            String firstLine = input.readLine();
+            String[] parts = firstLine.split(" ");
+            System.out.println(firstLine); // выводим пока есть что выводить
+            System.out.println(" parts[1]: " + parts[1]); // выводим пока есть что выводить
+
+
+            while (input.ready()) {
+                System.out.println(input.readLine()); // выводим пока есть что выводить
+            }
+
+
+            parts[1] = parts[1].substring(0);
+            Path path = Paths.get("C:\\REPOSITORY\\GB.JavaWeb.WebSerwer\\www\\", parts[1]);
+            //Path path = Paths.get(".", parts[1]);
+            if (!Files.exists(path)) {
+                output.println("HTTP/1.1 404 NOT_FOUND");
+                output.println("Content-Type: text/html; charset=utf-8");
+                output.println();
+                output.println("<h1>Файл " + parts[1] + " не найден</h1>"); //страница которую мы передаем
+                output.flush();
+
+            }
+            else {
+                //Ответ (версия протокола) (код 200) (текстовое значение кода)
+                //тип страницы и кодировка
+                //Обязательно пустую строку в конце (ответ закончен)
+                output.println("HTTP/1.1 200 OK");
+                output.println("Content-Type: text/html; charset=utf-8");
+                output.println();
+
+                Files.newBufferedReader(path).transferTo(output); //страница которую мы передаем
+                //output.println("<h1>Мой документ</h1>"); //страница которую мы передаем
+                output.flush();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
