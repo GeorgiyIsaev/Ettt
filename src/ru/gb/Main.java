933a325535a1a8bb0941ee1dadcb738f2bb91a69
Георгий ГераМера <germesutan@gmail.com>
2022-11-16 21:01:31 +0300

    страница

--- src/ru/gb/Main.java
+++ src/ru/gb/Main.java
@@ -1,8 +1,12 @@
 package ru.gb;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
 
@@ -14,6 +18,37 @@ public class Main {
             //Входим на страницу http://localhost:8080/
             Socket socket = serverSocket.accept(); //Будит ожидать подключение клиента
             System.out.println("New client connect");
+
+
+            //Получаемая от клиента информация socket.getInputStream() приходит в виде байтов
+            //Её можно преобразовать в читаемы вид через InputStreamReader
+            //класс BufferedReader позволяет считывать всю информацию в виде строки, а не символов
+            try(BufferedReader input =  new BufferedReader(
+                new InputStreamReader(
+                        socket.getInputStream(), StandardCharsets.UTF_8));
+                PrintWriter output = new PrintWriter(socket.getOutputStream());)
+            {
+
+                //input.ready(); // ожидаем прихода информации
+
+                while (!input.ready()); //ждем пока не появится что-то что можно считать
+                while (input.ready()){
+                    System.out.println(input.readLine()); // выводим пока есть что выводить
+                }
+
+                //Ответ (версия протокола) (код 200) (текстовое значение кода)
+                //тип страницы и кодировка
+                //Обязательно пустую строку в конце (ответ закончен)
+                output.println("HTTP/1.1 200 OK");
+                output.println("Content-Type: text/html; charset=utf-8");
+                output.println();
+                output.println("<h1>Мой документ</h1>"); //страница которую мы передаем
+
+
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
 
         } catch (IOException e){
             e.printStackTrace();
